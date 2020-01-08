15eaeda3d80ecbb00d14e4198ee66ace9459c037
Hyunuk <hyunuk@alumni.ubc.ca>
2020-01-08 00:50:50 -0800

    add scripts folder

---
+++ Assets/Scripts/Button.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Button : MonoBehaviour {
+    public Button button;
+    public Text buttonText;
+    public string playerSide;
+    public void SetSpace() {
+        buttonText.text = playerSide;
+        button.button = false;
+    }
+}
