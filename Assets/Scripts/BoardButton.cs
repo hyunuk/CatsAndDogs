3be3133587d7fb1f92c635892e5598d4ac62d619
I519061 <hyunuk.lim@sap.com>
2020-01-09 19:31:39 -0800

    change a button name to solve a bug

---
+++ Assets/Scripts/BoardButton.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BoardButton : MonoBehaviour {
+    public Button button;
+    public Text buttonText;
+    public Sprite sprite;
+    public string playerSide;
+    public void SetSpace() {
+        buttonText.text = playerSide;
+        //sprite.texture
+        button.interactable = false;
+
+    }
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    public Text[] buttonList;
+}
